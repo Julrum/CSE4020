791b392f5a0793ff6ee426038d48252a99755f95
박준원 <julrumg@gmail.com>
2021-03-30 14:58:27 +0900

    WIP:Modify main.py

--- ClassAssignment1/main.py
+++ ClassAssignment1/main.py
@@ -5,49 +5,49 @@ import numpy as np
 
 gXAng = 0.
 gYAng = 0.
-gZAng = 0.
+gXTrans = 0.
+gYTrans = 0.
+gZTrans = 0.
 cx = 0.
-cy = 0
-lx = 0
-ly = 0.
+cy = 0.
+lAx = 0.
+lAy = 0.
+lTx = 0.
+lTy = 0.
 leftButton = 0
 rightButton = 0
 
-# draw a cube of side 1, centered at the origin.
-def drawGrid(M):
+def drawGrid():
   glBegin(GL_LINES)
-  glVertex3fv(M @ np.array([ 0., 0., 0.]))
-  glVertex3fv(M @ np.array([20., 0., 0.]))
-  glVertex3fv(M @ np.array([ 0., 0., 0.]))
-  glVertex3fv(M @ np.array([ 0., 0.,20.]))
+  glVertex3f( 0., 0., 0.)
+  glVertex3f(20., 0., 0.)
+  glVertex3f( 0., 0., 0.)
+  glVertex3f( 0., 0.,20.)
   glEnd()
-  glRotatef(45, 0, 0, 1);
-  glRotatef(90, 0, 1, 0);
-  glTranslatef(-1, -1, 0);
-
-def drawGridArray(M):
+  
+def drawGridArray():
   for i in range(-20, 20):
     for j in range(-20, 20):
       glPushMatrix()
       glTranslatef(i,0, j)
-      drawGrid(M)
+      drawGrid()
       glPopMatrix()
 
-def drawFrame(M):
+def drawFrame():
   glBegin(GL_LINES)
   glColor3ub(255, 0, 0)
-  glVertex3fv(M @ np.array([ 0.,0.,0.]))
-  glVertex3fv(M @ np.array([20.,0.,0.]))
+  glVertex3f( 0.,0.,0.)
+  glVertex3f(20.,0.,0.)
   glColor3ub(0, 255, 0)
-  glVertex3fv(M @ np.array([0.,0.,0.]))
-  glVertex3fv(M @ np.array([0.,20.,0.]))
+  glVertex3f(0.,0.,0.)
+  glVertex3f(0.,20.,0.)
   glColor3ub(0, 0, 255)
-  glVertex3fv(M @ np.array([0.,0.,0]))
-  glVertex3fv(M @ np.array([0.,0.,20.]))
+  glVertex3f(0.,0.,0)
+  glVertex3f(0.,0.,20.)
   glEnd()
 
-def render(M):
-  global gXAng, gYAng, gZAng
+def render():
+  global gXAng, gYAng, gXTrans, gYTrans, gZTrans
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
   glEnable(GL_DEPTH_TEST)
   glPolygonMode( GL_FRONT_AND_BACK, GL_LINE )
@@ -57,53 +57,51 @@ def render(M):
   # test other parameter values
   glOrtho(-10,10, -10,10, -10,10)
 
-  # gluLookAt(1*np.sin(gCamAng),gCamHeight,1*np.cos(gCamAng), 0,0,0, 0,1,0)
-  
   glRotatef(gXAng, 1, 0, 0)
   glRotatef(gYAng, 0, 1, 0)
-  glRotatef(gZAng, 0, 0, 1)
-  glTranslatef(0, 0, 0)
-  drawFrame(M)
+  glTranslatef(gXTrans, gYTrans, gZTrans)
+  drawFrame()
   glColor3ub(255, 255, 255)
 
-  # drawGrid()
-
-  # test 
-
-  drawGridArray(M)
+  drawGridArray()
 
 def cursor_callback(window, xpos, ypos):
-  global gXAng, gYAng, cx, cy, lx, ly, leftButton, rightButton
-  if leftButton == 0:
+  global gXAng, gYAng, cx, cy, lAx, lAy, lTx, lTy, leftButton, rightButton, gXTrans, gYTrans
+  if leftButton == 0 and rightButton == 0:
     cx = xpos
     cy = ypos
-    print('x: %d, y: %d'%(cx, cy))
   elif leftButton == 1:
     dx = cx - xpos
     dy = cy - ypos
-    print('dx: %d dy: %d'%(dx, dy))
-    gYAng = dx
-    gXAng = dy
+    gYAng = lAx + dx
+    gXAng = lAy + dy
+  elif rightButton == 1:
+    dx = cx - xpos
+    dy = cy - ypos
+    gXTrans = lTx + dx*0.0416
+    gYTrans = lTy + dy*0.0416
   
 def button_callback(window, button, action, mod):
-  global gXAng, gYAng, leftButton, rightButton
+  global gXAng, gYAng, gXTrans, gYTrans, leftButton, rightButton, lAx, lAy, lTx, lTy, dx, dy
   if button==glfw.MOUSE_BUTTON_LEFT:
     if action==glfw.PRESS:
       leftButton = 1
-      print('press left btn: (%d, %d)'%glfw.get_cursor_pos(window))
     elif action==glfw.RELEASE:
-      print('release left btn: (%d, %d)'%glfw.get_cursor_pos(window))
       leftButton = 0
+      lAx = gYAng
+      lAy = gXAng
   if button==glfw.MOUSE_BUTTON_RIGHT:
     if action==glfw.PRESS:
       rightButton = 1
-      print('press left btn: (%d, %d)'%glfw.get_cursor_pos(window))
     elif action==glfw.RELEASE:
-      print('release left btn: (%d, %d)'%glfw.get_cursor_pos(window))
       rightButton = 0
+      lTx = gXTrans
+      lTy = gYTrans
 
 def scroll_callback(window, xoffset, yoffset):
-  print('mouse wheel scroll: %d, %d'%(xoffset, yoffset))
+  global gXTrans, gYTrans, gZTrans
+  gZTrans += yoffset
+  print('mouse wheel scroll: %f, %f, %f'%(gXTrans, gYTrans, gZTrans))
 
 def key_callback(window, key, scancode, action, mods):
   global gXAng, gYAng, gZAng
@@ -132,14 +130,9 @@ def main():
   glfw.set_mouse_button_callback(window, button_callback)
   glfw.set_scroll_callback(window, scroll_callback)
 
-
   while not glfw.window_should_close(window):
     glfw.poll_events()
-    # gluLookAt(1*np.sin(gCamAng),gCamHeight,1*np.cos(gCamAng), 0,0,0, 0,1,0)
-    render(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]]))
-    glRotatef(45, 0, 0, 1)
-    glRotatef(90, 0, 1, 0)
-    glTranslatef(-1, -1, 0)
+    render()
     glfw.swap_buffers(window)
 
   glfw.terminate()
